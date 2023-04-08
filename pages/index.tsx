d39c12318061e20a7f8790aea5bd08e600a1119a
MatsunamiDaiki <lax.md0@gmail.com>
2023-04-08 16:07:52 +0900

    ログイン画面作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { useEffect } from 'react'
+import useStore from '../store'
+import { supabase } from '../utils/supabase'
+import { Layout } from '../components/Layout'
+import { Auth } from '../components/Auth'
+import { DashBoard } from '../components/DashBoard'
 
 const Home: NextPage = () => {
+  const session = useStore((state) => state.session)
+  const setSession = useStore((state) => state.setSession)
+  useEffect(() => {
+    setSession(supabase.auth.session())
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
   return (
-    <div>
-      
-    </div>
+    <Layout title="Dashboard">{!session ? <Auth /> : <DashBoard />}</Layout>
   )
 }
 
