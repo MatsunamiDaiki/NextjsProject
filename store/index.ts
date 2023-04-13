d7c7dc11a4e891bc2f09a6a65f3acac58900a3d2
MatsunamiDaiki <lax.md0@gmail.com>
2023-04-13 13:37:08 +0900

    カスタムフック作成

--- store/index.ts
+++ store/index.ts
@@ -1,6 +1,6 @@
 import create from 'zustand'
 import { Session } from '@supabase/supabase-js'
-import { EditedProfile } from '../types'
+import { EditedNotice, EditedPost, EditedProfile } from '../types'
 
 type State = {
   session: Session | null
@@ -8,6 +8,12 @@ type State = {
   editedProfile: EditedProfile
   updateEditedProfile: (payload: EditedProfile) => void
   resetEditedProfile: () => void
+  editedNotice: EditedNotice
+  updateEditedNotice: (payload: EditedNotice) => void
+  resetEditedNotice: () => void
+  editedPost: EditedPost
+  updateEditedPost: (payload: EditedPost) => void
+  resetEditedPost: () => void
 }
 
 const useStore = create<State>((set) => ({
@@ -24,6 +30,26 @@ const useStore = create<State>((set) => ({
     }),
   resetEditedProfile: () =>
     set({ editedProfile: { username: '', favorites: '', avatar_url: '' } }),
+    editedNotice: { id: '', content: '' },
+  updateEditedNotice: (payload) =>
+    set({
+      editedNotice: {
+        id: payload.id,
+        content: payload.content,
+      },
+    }),
+  resetEditedNotice: () => set({ editedNotice: { id: '', content: '' } }),
+  editedPost: { id: '', title: '', post_url: '' },
+  updateEditedPost: (payload) =>
+    set({
+      editedPost: {
+        id: payload.id,
+        title: payload.title,
+        post_url: payload.post_url,
+      },
+    }),
+  resetEditedPost: () =>
+    set({ editedPost: { id: '', title: '', post_url: '' } }),
 }))
 
 export default useStore
